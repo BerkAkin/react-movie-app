23e4f38759f663e5620e9f9f9d2b66904fbb7602
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-12 12:28:18 +0300

    style: bootstrap included to main component for usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import HomePage from "./components/HomePage/HomePage";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import MovieDetail from "./components/MovieDetail/MovieDetail";
+import HomePage from "./components/HomePage/HomePage";
+
+import NavbarComponent from "./components/Navbar/NavbarComponent";
+import FooterComponent from "./components/Footer/FooterComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 function App() {
   return (
     <div className="App">
+      <NavbarComponent />
       <Router>
         <Routes>
           <Route path="/movieDetail/:id" element={<MovieDetail />}></Route>
           <Route path="/" element={<HomePage />}></Route>
         </Routes>
       </Router>
+      <FooterComponent />
     </div>
   );
 }
