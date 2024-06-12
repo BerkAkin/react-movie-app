d486889eeb72e45f19da8832878a7e60021f76c0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-12 07:59:41 +0300

    feat: added routing system for moviedetail and homepage for spa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import HomePage from "./components/HomePage/HomePage";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/movieDetail/:id" element={<MovieDetail />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
