02c69a39289ed407adfa36c2e25a98523833c5b3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-12 14:51:14 +0300

    FEAT: Homepage is now rendered by container

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Container from "./components/Container/Container";
 
-import MovieDetail from "./components/MovieDetail/MovieDetail";
-import HomePage from "./components/HomePage/HomePage";
-
-import NavbarComponent from "./components/Navbar/NavbarComponent";
-import FooterComponent from "./components/Footer/FooterComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
-      <Router>
-        <Routes>
-          <Route path="/movieDetail/:id" element={<MovieDetail />}></Route>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </Router>
-      <FooterComponent />
+      <Container />
     </div>
   );
 }
