3dac81f357c357d4b11cbe2664ca4f5e65dd8956
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-12 14:52:48 +0300

    feat: card design implemented for homepage to show movies

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+
+interface Props {
+  movie: {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+  };
+}
+
+function Card(props: Props) {
+  const { movie } = props;
+  const backdropUrl = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+  return (
+    <>
+      <div className={`${styles.cardBody} d-flex flex-column justify-content-start`}>
+        <div className={styles.cardImagePanel}>
+          <Link className="text-decoration-none text-dark fw-bold" to={`/movieDetail/${movie.id}`}>
+            <img style={{ borderRadius: "10px" }} src={`${backdropUrl}`} width={200} />
+          </Link>
+        </div>
+        <div className={`${styles.cardTitlePanel} text-center pt-2 `}>
+          <Link className="text-decoration-none text-dark fw-bold" to={`/movieDetail/${movie.id}`}>
+            {movie.title}
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
