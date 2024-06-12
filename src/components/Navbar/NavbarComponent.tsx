c47a36487e785f2daae4940a990e83d4eba7f1fd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-12 14:54:03 +0300

    feat: navbar is now implemented and working properly

--- src/components/Navbar/NavbarComponent.tsx
+++ src/components/Navbar/NavbarComponent.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-
-function FooterComponent() {
+import styles from "./styles.module.css";
+function NavbarComponent() {
   return (
     <>
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid  ">
-            <a className="navbar-brand fs-1 " href="#">
-              MovieMania
+        <nav className={`fixed-top navbar navbar-expand-lg navbar-dark ${styles.bgBlur}`}>
+          <div className="container-fluid ">
+            <a className="navbar-brand fs-1 ms-2" href="#" style={{ letterSpacing: "0.5rem" }}>
+              MovieManiac
             </a>
             <button
               className="navbar-toggler"
@@ -24,15 +23,30 @@ function FooterComponent() {
               <ul className=" navbar-nav  me-auto mb-2 mb-lg-0">
                 <li className=" nav-item ">
                   <a className="nav-link ms-4 mt-2" aria-current="page" href="#">
-                    FİLMLER
+                    YENİ FİLMLER
+                  </a>
+                </li>
+                <li className=" nav-item ">
+                  <a className="nav-link ms-4 mt-2" aria-current="page" href="#">
+                    TREND FİLMLER
+                  </a>
+                </li>
+                <li className=" nav-item ">
+                  <a className="nav-link ms-4 mt-2" aria-current="page" href="#">
+                    TÜRLER
+                  </a>
+                </li>
+                <li className=" nav-item ">
+                  <a className="nav-link ms-4 mt-2" aria-current="page" href="#">
+                    DİZİLER
                   </a>
                 </li>
               </ul>
               <div className="d-flex justify-content-center align-items-center">
-                <a className="w-100 btn-dark btn btn-lg fs-5 text-nowrap" style={{ height: "90px", borderRadius: 0, paddingTop: "30px" }}>
+                <a className="w-100 btn btn-lg fs-5 text-nowrap text-light" style={{ height: "90px", borderRadius: 0, paddingTop: "30px" }}>
                   OTURUM AÇ
                 </a>
-                <a className="w-100 btn-dark btn btn-lg fs-5" style={{ height: "90px", borderRadius: 0, paddingTop: "30px" }}>
+                <a className="w-100 btn btn-lg fs-5 text-light" style={{ height: "90px", borderRadius: 0, paddingTop: "30px" }}>
                   ÜYE OL
                 </a>
               </div>
@@ -44,4 +58,4 @@ function FooterComponent() {
   );
 }
 
-export default FooterComponent;
+export default NavbarComponent;
