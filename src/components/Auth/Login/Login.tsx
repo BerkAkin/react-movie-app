04596d9e6d1f26e2e0f0e459a4f5c201904d535d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-13 11:45:10 +0300

    feat: login component implemented and now working

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Login() {
-  return <>Login</>;
+interface User {
+  id: number;
+  username: string;
 }
+
+const Login: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("http://localhost:5000/api/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className=" mt-5 pt-5">
+        {users.map((user) => (
+          <div key={user.id}>
+            {user.id}: {user.username}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Login;
