2c36167491f292df7ba4939005e033466973ae44
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-13 13:12:01 +0300

    style: form style of login is now live

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Field } from "formik";
+import styles from "../style.module.css";
 
-interface User {
-  id: number;
-  username: string;
-}
-
-const Login: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    axios
-      .get<User[]>("http://localhost:5000/api/users")
-      .then((response) => {
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+function Login() {
+  useEffect(() => {}, []);
 
   return (
     <>
-      <div className=" mt-5 pt-5">
-        {users.map((user) => (
-          <div key={user.id}>
-            {user.id}: {user.username}
+      <div className="vh-100 d-flex justify-content-center align-items-center">
+        <div className={`container ${styles.registerBody}`}>
+          <div className="row" style={{ height: "100%" }}>
+            <div className="col-6">
+              <div className="row h-100 text-center d-flex text-light align-items-center">
+                <p style={{ fontSize: "7rem" }} className="display-1">
+                  CINEMOVES
+                </p>
+              </div>
+            </div>
+            <div className={`col-6 d-flex flex-column`} style={{ color: "#b5b5b5" }}>
+              <div className="row d-flex justify-content-center flex-column align-items-center my-auto  h-75 ">
+                <input placeholder="Kullanıcı Adı" className={`mt-5 text-center ${styles.input}`}></input>
+                <input placeholder="Şifre" type="password" className={`mt-5 text-center ${styles.input}`}></input>
+              </div>
+              <div className="row d-flex justify-content-center flex-column align-items-center h-25">
+                <button className={` ${styles.button}`}>Giriş Yap</button>
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
-};
+}
 
 export default Login;
