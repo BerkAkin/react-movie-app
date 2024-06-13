a34c1c075421c0b421c36e5c8a87f5f373e6d0ea
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-13 06:49:25 +0300

    feat: added air date and release date informations

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,6 +8,8 @@ interface Props {
     poster_path: string;
     name?: string;
     title?: string;
+    first_air_date?: string;
+    release_date?: string;
   };
   category?: string;
 }
@@ -19,14 +21,17 @@ function Card(props: Props) {
     <>
       <div className={`${styles.cardBody} d-flex flex-column justify-content-start`}>
         <div className={styles.cardImagePanel}>
-          <Link className="text-decoration-none text-dark fw-bold" to={`/${category}Detail/${movie.id}`}>
+          <Link to={`/${category}Detail/${movie.id}`}>
             <img style={{ borderRadius: "10px" }} src={`${backdropUrl}`} width={200} />
           </Link>
         </div>
-        <div className={`${styles.cardTitlePanel} text-center pt-2 `}>
-          <Link className="text-decoration-none text-dark fw-bold" to={`/${category}Detail/${movie.id}`}>
+        <div className={`${styles.cardTitlePanel} text-center my-auto pt-2 `}>
+          <Link className="text-decoration-none text-light  fw-bold" to={`/${category}Detail/${movie.id}`}>
             {movie.name ? movie.name : movie.title}
           </Link>
+        </div>
+        <div className={`${styles.cardTitlePanel} text-center text-light my-auto `}>
+          {category === "series" ? `${movie.first_air_date?.substring(0, 4)}` : `${movie.release_date?.substring(0, 4)}`}
         </div>
       </div>
     </>
