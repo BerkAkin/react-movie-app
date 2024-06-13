e5bcc94e70a61d42797169944e30d0519eba3b38
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-13 05:53:50 +0300

    feat: category informaiton is now sending with url

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,21 +9,22 @@ interface Props {
     name?: string;
     title?: string;
   };
+  category?: string;
 }
 
 function Card(props: Props) {
-  const { movie } = props;
+  const { movie, category } = props;
   const backdropUrl = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
   return (
     <>
       <div className={`${styles.cardBody} d-flex flex-column justify-content-start`}>
         <div className={styles.cardImagePanel}>
-          <Link className="text-decoration-none text-dark fw-bold" to={`/movieDetail/${movie.id}`}>
+          <Link className="text-decoration-none text-dark fw-bold" to={`/${category}Detail/${movie.id}`}>
             <img style={{ borderRadius: "10px" }} src={`${backdropUrl}`} width={200} />
           </Link>
         </div>
         <div className={`${styles.cardTitlePanel} text-center pt-2 `}>
-          <Link className="text-decoration-none text-dark fw-bold" to={`/movieDetail/${movie.id}`}>
+          <Link className="text-decoration-none text-dark fw-bold" to={`/${category}Detail/${movie.id}`}>
             {movie.name ? movie.name : movie.title}
           </Link>
         </div>
