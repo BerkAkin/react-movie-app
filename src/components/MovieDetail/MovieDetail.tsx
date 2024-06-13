b8ff71de77a45af99141ab292bd96fa45d46b49b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-13 05:55:00 +0300

    feat: serie details are now more detailed

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./styles.module.css";
-import { title } from "process";
 
 const api = process.env.REACT_APP_API_KEY;
 
@@ -14,7 +13,9 @@ function MovieDetail() {
       Authorization: `Bearer ${api}`,
     },
   };
-  const { id } = useParams();
+
+  const { id, seriesDetail } = useParams();
+  console.log(seriesDetail);
   const [movieDetail, setMovieDetail] = useState<{
     backdrop_path: string;
     poster_path: string;
@@ -22,12 +23,35 @@ function MovieDetail() {
     overview: string;
     release_date: string;
     genres: { id: number; name: string }[];
-  }>({ backdrop_path: "", poster_path: "", title: "", overview: "", genres: [], release_date: "" });
+    name: string;
+    episode_run_time: string;
+    number_of_seasons: string;
+    first_air_date: string;
+    created_by?: [{ name: string }];
+  }>({
+    backdrop_path: "",
+    poster_path: "",
+    title: "",
+    overview: "",
+    genres: [],
+    release_date: "",
+    name: "",
+    number_of_seasons: "",
+    episode_run_time: "",
+    first_air_date: "",
+    created_by: [{ name: "" }],
+  });
   const posterUrl = `https://image.tmdb.org/t/p/original/${movieDetail.backdrop_path}`;
   const backdropUrl = `https://image.tmdb.org/t/p/original/${movieDetail.poster_path}`;
 
   useEffect(() => {
-    axios(`https://api.themoviedb.org/3/movie/${id}?language=tr-TR`, options)
+    let url = ``;
+    if (seriesDetail === "seriesDetail") {
+      url = `https://api.themoviedb.org/3/tv/${id}?language=tr-TR`;
+    } else {
+      url = `https://api.themoviedb.org/3/movie/${id}?language=tr-TR`;
+    }
+    axios(url, options)
       .then((response) => {
         setMovieDetail(response.data);
         console.log(response);
@@ -48,7 +72,7 @@ function MovieDetail() {
               </div>
               <div className="col-8">
                 <div style={{ fontSize: "5.1rem" }} className="d-flex text-white fw-bold mt-1">
-                  {movieDetail.title}
+                  {seriesDetail === "seriesDetail" ? `${movieDetail.name}` : `${movieDetail.title}`}
                 </div>
                 <div className="mt-4">
                   {movieDetail.genres.map((genre) => (
@@ -56,12 +80,24 @@ function MovieDetail() {
                       {genre.name}
                     </span>
                   ))}
-                </div>{" "}
-                <div style={{ fontSize: "1.3rem" }} className="d-flex text-white  mt-5">
-                  {movieDetail.overview}
                 </div>
-                <div className="d-flex text-white  mt-5">
-                  Çıkış Tarihi: <span className="ms-1"> {movieDetail.release_date}</span>
+                <div style={{ fontSize: "1.05rem" }} className="d-flex text-white  mt-3">
+                  {movieDetail.overview}
+                </div>{" "}
+                <div style={{ fontSize: "1.05rem" }} className="d-flex text-white  mt-4">
+                  {seriesDetail === "seriesDetail" &&
+                    movieDetail.created_by &&
+                    movieDetail.created_by.length > 0 &&
+                    `Yapımcı: ${movieDetail.created_by[0].name}`}
+                </div>{" "}
+                <div style={{ fontSize: "1.05rem" }} className="d-flex text-white  mt-1">
+                  {seriesDetail === "seriesDetail" ? `İlk Yayın Tarihi: ${movieDetail.first_air_date}` : ""}
+                </div>{" "}
+                <div style={{ fontSize: "1.05rem" }} className="d-flex text-white  mt-1">
+                  {seriesDetail === "seriesDetail" ? `Sezon Sayısı: ${movieDetail.number_of_seasons}` : `Çıkış Tarihi:  ${movieDetail.release_date}`}
+                </div>{" "}
+                <div style={{ fontSize: "1.05rem" }} className="d-flex text-white  mt-1">
+                  {seriesDetail === "seriesDetail" ? `Bölüm Uzunluğu: ${movieDetail.episode_run_time} Dk` : ""}
                 </div>
               </div>
             </div>
